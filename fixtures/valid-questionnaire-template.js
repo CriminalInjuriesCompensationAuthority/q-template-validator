7c4baf3b83ac0163be3f1cb57a8acede5d2b746e
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2023-01-25 16:08:23 +0000

    Add support for `user` schema

--- fixtures/valid-questionnaire-template.js
+++ fixtures/valid-questionnaire-template.js
@@ -6828,6 +6828,36 @@ const validQTemplate = {
                 ]
             }
         },
+        user: {
+            schema: {
+                $schema: 'http://json-schema.org/draft-07/schema#',
+                type: 'object',
+                required: ['user-id'],
+                properties: {
+                    'user-id': {
+                        type: 'string',
+                        pattern: '^urn:fdc:gov.uk:2022:[A-Za-z0-9+/=_-]{1,44}$',
+                        errorMessage: {
+                            pattern: 'Invalid user id'
+                        }
+                    }
+                },
+                errorMessage: {
+                    required: 'User id is required'
+                },
+                examples: [
+                    {
+                        'user-id':
+                            'urn:fdc:gov.uk:2022:56P4CMsGh_02YOlWpd8PAOI-2sVlB2nsNU7mcLZYhYw='
+                    }
+                ],
+                invalidExamples: [
+                    {
+                        'user-id': 12345
+                    }
+                ]
+            }
+        },
         'p--context-crime-impact': {
             l10n: {
                 vars: {
@@ -9825,6 +9855,9 @@ const validQTemplate = {
                 }
             },
             system: {
+                type: 'final'
+            },
+            user: {
                 type: 'final'
             },
             'p--context-crime-impact': {
